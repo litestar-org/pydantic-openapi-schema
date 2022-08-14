00a73b422b6f55d78950eb392d20c94b07159ae4
Cody Fincher <cody.fincher@gmail.com>
2022-08-14 11:08:31 -0500

    FEAT: Added str to validator so that pylint does not report a linting error when passing a string instead of EmailStr.

--- pydantic_openapi_schema/v3_0_3/contact.py
+++ pydantic_openapi_schema/v3_0_3/contact.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, Union
 
-from pydantic import AnyUrl, BaseModel, EmailStr, Extra
+from pydantic import AnyUrl, BaseModel, EmailStr, Extra, validator
 
 
 class Contact(BaseModel):
@@ -18,10 +18,29 @@ class Contact(BaseModel):
     The URL pointing to the contact information. MUST be in the format of a URL.
     """
 
-    email: Optional[EmailStr] = None
+    email: Optional[Union[EmailStr,str]] = None
     """
     The email address of the contact person/organization. MUST be in the format of an email address.
     """
+    @validator("email", pre=True)
+    def validate_email(  # pylint: disable=no-self-argument
+        cls, v: Union[EmailStr, str],
+    ) -> EmailStr:
+        """Validates that email is a valid email address
+
+        Args:
+            v (EmailStr|str): Holds the email string to be validated
+
+        Raises:
+            ValueError: Value is not a valid email address
+
+        Returns:
+            _type_: CompressionBackend
+        """
+        if isinstance(v, str):
+            v = EmailStr(v)
+        return v
+
 
     class Config:
         extra = Extra.ignore
--- pydantic_openapi_schema/v3_1_0/contact.py
+++ pydantic_openapi_schema/v3_1_0/contact.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, Union
 
-from pydantic import AnyUrl, BaseModel, EmailStr, Extra
+from pydantic import AnyUrl, BaseModel, EmailStr, Extra, validator
 
 
 class Contact(BaseModel):
@@ -24,6 +24,24 @@ class Contact(BaseModel):
     The email address of the contact person/organization.
     MUST be in the form of an email address.
     """
+    @validator("email", pre=True)
+    def validate_email(  # pylint: disable=no-self-argument
+        cls, v: Union[EmailStr, str],
+    ) -> EmailStr:
+        """Validates that email is a valid email address
+
+        Args:
+            v (EmailStr|str): Holds the email string to be validated
+
+        Raises:
+            ValueError: Value is not a valid email address
+
+        Returns:
+            _type_: CompressionBackend
+        """
+        if isinstance(v, str):
+            v = EmailStr(v)
+        return v
 
     class Config:
         extra = Extra.ignore
