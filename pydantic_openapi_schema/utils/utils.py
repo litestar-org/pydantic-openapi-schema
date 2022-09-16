706b92268421c580c2c6707ec7643e93db40279e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-16 21:36:16 +0200

    Support customizing component name for pydantic models (#12)
    * support customizing pydantic model name in component schema
    
    * added constraint to pydantic
    
    * refactor

--- pydantic_openapi_schema/utils/utils.py
+++ pydantic_openapi_schema/utils/utils.py
@@ -1,6 +1,6 @@
-from typing import TYPE_CHECKING, Any, List, Optional, Set, Type, TypeVar, cast
+from typing import TYPE_CHECKING, Any, Set, Type, TypeVar, cast
 
-from pydantic import BaseModel
+from pydantic import BaseModel, create_model
 from pydantic.schema import schema
 
 from pydantic_openapi_schema import v3_1_0
@@ -9,6 +9,7 @@ if TYPE_CHECKING:
     from typing import Dict
 
 REF_PREFIX = "#/components/schemas/"
+SCHEMA_NAME_ATTRIBUTE = "__schema_name__"
 
 T = TypeVar("T", bound=v3_1_0.OpenAPI)
 
@@ -22,45 +23,35 @@ class OpenAPI310PydanticSchema(v3_1_0.Schema):
 
 def construct_open_api_with_schema_class(
     open_api_schema: T,
-    schema_classes: Optional[List[Type[BaseModel]]] = None,
-    scan_for_pydantic_schema_reference: bool = True,
-    by_alias: bool = True,
 ) -> T:
     """Construct a new OpenAPI object, with the use of pydantic classes to
     produce JSON schemas.
 
     Args:
-        open_api_schema: the base `OpenAPI` object
-        schema_classes: pydantic classes that their schema will be used as "#/components/schemas" values
-        scan_for_pydantic_schema_reference: flag to indicate if scanning for `PydanticSchemaReference`
-            class is needed for "#/components/schemas" value updates
-        by_alias: construct schema by alias (default is True)
+        open_api_schema: An instance of the OpenAPI model.
 
     Returns:
         new OpenAPI object with "#/components/schemas" values updated. If there is no update in
             "#/components/schemas" values, the original `open_api` will be returned.
     """
     copied_schema = open_api_schema.copy(deep=True)
-
-    if scan_for_pydantic_schema_reference:
-        extracted_schema_classes = extract_pydantic_types_to_openapi_components(
-            obj=copied_schema, ref_class=v3_1_0.Reference
-        )
-        schema_classes = list(
-            {*schema_classes, *extracted_schema_classes} if schema_classes else extracted_schema_classes
-        )
+    schema_classes = list(extract_pydantic_types_to_openapi_components(obj=copied_schema, ref_class=v3_1_0.Reference))
 
     if not schema_classes:
         return open_api_schema
 
     if not copied_schema.components:
         copied_schema.components = v3_1_0.Components(schemas={})
-    elif not copied_schema.components.schemas:
+    if copied_schema.components.schemas is None:  # pragma: no cover
         copied_schema.components.schemas = cast("Dict[str, Any]", {})
 
+    schema_classes = [
+        cls if not hasattr(cls, "__schema_name__") else create_model(getattr(cls, SCHEMA_NAME_ATTRIBUTE), __base__=cls)
+        for cls in schema_classes
+    ]
     schema_classes.sort(key=lambda x: x.__name__)
-    schema_definitions = schema(schema_classes, by_alias=by_alias, ref_prefix=REF_PREFIX)["definitions"]
-    copied_schema.components.schemas.update(  # type: ignore
+    schema_definitions = schema(schema_classes, ref_prefix=REF_PREFIX)["definitions"]
+    copied_schema.components.schemas.update(
         {key: v3_1_0.Schema.parse_obj(schema_dict) for key, schema_dict in schema_definitions.items()}
     )
     return copied_schema
@@ -84,22 +75,34 @@ def extract_pydantic_types_to_openapi_components(obj: Any, ref_class: Type[v3_1_
         for field in fields:
             child_obj = getattr(obj, field)
             if isinstance(child_obj, OpenAPI310PydanticSchema):
-                setattr(obj, field, ref_class(ref=REF_PREFIX + child_obj.schema_class.__name__))
+                setattr(obj, field, ref_class(ref=create_ref_prefix(child_obj.schema_class)))
                 pydantic_schemas.add(child_obj.schema_class)
             else:
                 pydantic_schemas.update(extract_pydantic_types_to_openapi_components(child_obj, ref_class=ref_class))
     elif isinstance(obj, list):
         for index, elem in enumerate(obj):
             if isinstance(elem, OpenAPI310PydanticSchema):
-                obj[index] = ref_class(ref=REF_PREFIX + elem.schema_class.__name__)
+                obj[index] = ref_class(ref=create_ref_prefix(elem.schema_class))
                 pydantic_schemas.add(elem.schema_class)
             else:
                 pydantic_schemas.update(extract_pydantic_types_to_openapi_components(elem, ref_class=ref_class))
     elif isinstance(obj, dict):
         for key, value in obj.items():
             if isinstance(value, OpenAPI310PydanticSchema):
-                obj[key] = ref_class(ref=REF_PREFIX + value.schema_class.__name__)
+                obj[key] = ref_class(ref=create_ref_prefix(value.schema_class))
                 pydantic_schemas.add(value.schema_class)
             else:
                 pydantic_schemas.update(extract_pydantic_types_to_openapi_components(value, ref_class=ref_class))
     return pydantic_schemas
+
+
+def create_ref_prefix(model: Type[BaseModel]) -> str:
+    """
+
+    Args:
+        model: Pydantic model instance.
+
+    Returns:
+        A prefixed name.
+    """
+    return REF_PREFIX + getattr(model, SCHEMA_NAME_ATTRIBUTE, model.__name__)
--- tests/v3_1_0/test_util.py
+++ tests/v3_1_0/test_util.py
@@ -10,47 +10,36 @@ from pydantic_openapi_schema.v3_1_0 import (
     OpenAPI,
     Operation,
     PathItem,
-    Reference,
     RequestBody,
     Response,
 )
+from tests.v3_1_0.utils import PingRequest as OtherPingRequest
+from tests.v3_1_0.utils import PingResponse as OtherPingResponse
 
 
-def test_construct_open_api_with_schema_class_1() -> None:
-    open_api = construct_base_open_api_1()
-    result_open_api_1 = construct_open_api_with_schema_class(open_api)
-    result_open_api_2 = construct_open_api_with_schema_class(open_api, [PingRequest, PingResponse])
-    assert result_open_api_1.components == result_open_api_2.components
-    assert result_open_api_1 == result_open_api_2
+class PingRequest(BaseModel):
+    """Ping Request."""
+
+    req_foo: str = Field(description="foo value of the request")
+    req_bar: str = Field(description="bar value of the request")
 
 
-def test_construct_open_api_with_schema_class_2() -> None:
-    open_api_1 = construct_base_open_api_1()
-    open_api_2 = construct_base_open_api_2()
-    result_open_api_1 = construct_open_api_with_schema_class(open_api_1)
-    result_open_api_2 = construct_open_api_with_schema_class(open_api_2, [PingRequest, PingResponse])
-    assert result_open_api_1 == result_open_api_2
+class PingResponse(BaseModel):
+    """Ping response."""
+
+    resp_foo: str = Field(description="foo value of the response")
+    resp_bar: str = Field(description="bar value of the response")
 
 
-def test_construct_open_api_with_schema_class_3() -> None:
-    open_api_3 = construct_base_open_api_3()
+class PongResponse(BaseModel):
+    """Pong response."""
 
-    result_with_alias_1 = construct_open_api_with_schema_class(open_api_3)
-    schema_with_alias = result_with_alias_1.components.schemas["PongResponse"]  # type: ignore
-    assert "pong_foo" in schema_with_alias.properties  # type: ignore
-    assert "pong_bar" in schema_with_alias.properties  # type: ignore
-
-    result_with_alias_2 = construct_open_api_with_schema_class(open_api_3)
-    assert result_with_alias_1 == result_with_alias_2
-
-    result_without_alias = construct_open_api_with_schema_class(open_api_3, by_alias=False)
-    schema_without_alias = result_without_alias.components.schemas["PongResponse"]  # type: ignore
-    assert "resp_foo" in schema_without_alias.properties  # type: ignore
-    assert "resp_bar" in schema_without_alias.properties  # type: ignore
+    resp_foo: str = Field(alias="pong_foo", description="foo value of the response")
+    resp_bar: str = Field(alias="pong_bar", description="bar value of the response")
 
 
-def construct_base_open_api_1() -> OpenAPI:
-    return OpenAPI.parse_obj(
+def test_construct_open_api_parse_obj() -> None:
+    open_api = OpenAPI.parse_obj(
         {
             "info": {"title": "My own API", "version": "v0.0.1"},
             "paths": {
@@ -74,42 +63,61 @@ def construct_base_open_api_1() -> OpenAPI:
             },
         }
     )
-
-
-def construct_base_open_api_2() -> OpenAPI:
-    return OpenAPI(
-        info=Info(
-            title="My own API",
-            version="v0.0.1",
-        ),
-        paths={
-            "/ping": PathItem(
-                post=Operation(
-                    requestBody=RequestBody(
-                        content={
-                            "application/json": MediaType(
-                                media_type_schema=Reference(ref="#/components/schemas/PingRequest")
-                            )
-                        }
-                    ),
-                    responses={
-                        "200": Response(
-                            description="pong",
-                            content={
-                                "application/json": MediaType(
-                                    media_type_schema=Reference(ref="#/components/schemas/PingResponse")
-                                )
-                            },
-                        )
+    result = construct_open_api_with_schema_class(open_api)
+    assert result.dict(exclude_none=True) == {
+        "openapi": "3.1.0",
+        "info": {"title": "My own API", "version": "v0.0.1"},
+        "servers": [{"url": "/"}],
+        "paths": {
+            "/ping": {
+                "post": {
+                    "requestBody": {
+                        "content": {
+                            "application/json": {"media_type_schema": {"ref": "#/components/schemas/PingRequest"}}
+                        },
+                        "required": False,
                     },
-                )
-            )
+                    "responses": {
+                        "200": {
+                            "description": "pong",
+                            "content": {
+                                "application/json": {"media_type_schema": {"ref": "#/components/schemas/PingResponse"}}
+                            },
+                        }
+                    },
+                    "deprecated": False,
+                }
+            }
         },
-    )
+        "components": {
+            "schemas": {
+                "PingRequest": {
+                    "properties": {
+                        "req_foo": {"type": "string", "title": "Req Foo", "description": "foo value of the request"},
+                        "req_bar": {"type": "string", "title": "Req Bar", "description": "bar value of the request"},
+                    },
+                    "type": "object",
+                    "required": ["req_foo", "req_bar"],
+                    "title": "PingRequest",
+                    "description": "Ping Request.",
+                },
+                "PingResponse": {
+                    "properties": {
+                        "resp_foo": {"type": "string", "title": "Resp Foo", "description": "foo value of the response"},
+                        "resp_bar": {"type": "string", "title": "Resp Bar", "description": "bar value of the response"},
+                    },
+                    "type": "object",
+                    "required": ["resp_foo", "resp_bar"],
+                    "title": "PingResponse",
+                    "description": "Ping response.",
+                },
+            }
+        },
+    }
 
 
-def construct_base_open_api_3() -> OpenAPI:
-    return OpenAPI(
+def test_construct_open_api_with_schema_class() -> None:
+    open_api = OpenAPI(
         info=Info(
             title="My own API",
             version="v0.0.1",
@@ -139,23 +147,212 @@ def construct_base_open_api_3() -> OpenAPI:
         },
     )
 
+    result = construct_open_api_with_schema_class(open_api)
+    assert result.dict(exclude_none=True) == {
+        "openapi": "3.1.0",
+        "info": {"title": "My own API", "version": "v0.0.1"},
+        "servers": [{"url": "/"}],
+        "paths": {
+            "/ping": {
+                "post": {
+                    "requestBody": {
+                        "content": {
+                            "application/json": {"media_type_schema": {"ref": "#/components/schemas/PingRequest"}}
+                        },
+                        "required": False,
+                    },
+                    "responses": {
+                        "200": {
+                            "description": "pong",
+                            "content": {
+                                "application/json": {"media_type_schema": {"ref": "#/components/schemas/PongResponse"}}
+                            },
+                        }
+                    },
+                    "deprecated": False,
+                }
+            }
+        },
+        "components": {
+            "schemas": {
+                "PingRequest": {
+                    "properties": {
+                        "req_foo": {"type": "string", "title": "Req Foo", "description": "foo value of the request"},
+                        "req_bar": {"type": "string", "title": "Req Bar", "description": "bar value of the request"},
+                    },
+                    "type": "object",
+                    "required": ["req_foo", "req_bar"],
+                    "title": "PingRequest",
+                    "description": "Ping Request.",
+                },
+                "PongResponse": {
+                    "properties": {
+                        "pong_foo": {"type": "string", "title": "Pong Foo", "description": "foo value of the response"},
+                        "pong_bar": {"type": "string", "title": "Pong Bar", "description": "bar value of the response"},
+                    },
+                    "type": "object",
+                    "required": ["pong_foo", "pong_bar"],
+                    "title": "PongResponse",
+                    "description": "Pong response.",
+                },
+            }
+        },
+    }
 
-class PingRequest(BaseModel):
-    """Ping Request."""
 
-    req_foo: str = Field(description="foo value of the request")
-    req_bar: str = Field(description="bar value of the request")
+def test_handling_of_models_with_same_name() -> None:
+    open_api = OpenAPI(
+        info=Info(
+            title="My own API",
+            version="v0.0.1",
+        ),
+        paths={
+            "/ping1": PathItem(
+                post=Operation(
+                    requestBody=RequestBody(
+                        content={
+                            "application/json": MediaType(
+                                media_type_schema=OpenAPI310PydanticSchema(schema_class=PingRequest)
+                            )
+                        }
+                    ),
+                    responses={
+                        "200": Response(
+                            description="pong",
+                            content={
+                                "application/json": MediaType(
+                                    media_type_schema=OpenAPI310PydanticSchema(schema_class=PingResponse)
+                                )
+                            },
+                        )
+                    },
+                )
+            ),
+            "/ping2": PathItem(
+                post=Operation(
+                    requestBody=RequestBody(
+                        content={
+                            "application/json": MediaType(
+                                media_type_schema=OpenAPI310PydanticSchema(schema_class=OtherPingRequest)
+                            )
+                        }
+                    ),
+                    responses={
+                        "200": Response(
+                            description="pong",
+                            content={
+                                "application/json": MediaType(
+                                    media_type_schema=OpenAPI310PydanticSchema(schema_class=OtherPingResponse)
+                                )
+                            },
+                        )
+                    },
+                )
+            ),
+        },
+    )
+    result = construct_open_api_with_schema_class(open_api)
+    assert result.dict(exclude_none=True) == {
+        "openapi": "3.1.0",
+        "info": {"title": "My own API", "version": "v0.0.1"},
+        "servers": [{"url": "/"}],
+        "paths": {
+            "/ping1": {
+                "post": {
+                    "requestBody": {
+                        "content": {
+                            "application/json": {"media_type_schema": {"ref": "#/components/schemas/PingRequest"}}
+                        },
+                        "required": False,
+                    },
+                    "responses": {
+                        "200": {
+                            "description": "pong",
+                            "content": {
+                                "application/json": {"media_type_schema": {"ref": "#/components/schemas/PingResponse"}}
+                            },
+                        }
+                    },
+                    "deprecated": False,
+                }
+            },
+            "/ping2": {
+                "post": {
+                    "requestBody": {
+                        "content": {
+                            "application/json": {
+                                "media_type_schema": {"ref": "#/components/schemas/RenamedPingRequest"}
+                            }
+                        },
+                        "required": False,
+                    },
+                    "responses": {
+                        "200": {
+                            "description": "pong",
+                            "content": {
+                                "application/json": {
+                                    "media_type_schema": {"ref": "#/components/schemas/RenamedPingResponse"}
+                                }
+                            },
+                        }
+                    },
+                    "deprecated": False,
+                }
+            },
+        },
+        "components": {
+            "schemas": {
+                "PingRequest": {
+                    "properties": {
+                        "req_foo": {"type": "string", "title": "Req Foo", "description": "foo value of the request"},
+                        "req_bar": {"type": "string", "title": "Req Bar", "description": "bar value of the request"},
+                    },
+                    "type": "object",
+                    "required": ["req_foo", "req_bar"],
+                    "title": "PingRequest",
+                    "description": "Ping Request.",
+                },
+                "PingResponse": {
+                    "properties": {
+                        "resp_foo": {"type": "string", "title": "Resp Foo", "description": "foo value of the response"},
+                        "resp_bar": {"type": "string", "title": "Resp Bar", "description": "bar value of the response"},
+                    },
+                    "type": "object",
+                    "required": ["resp_foo", "resp_bar"],
+                    "title": "PingResponse",
+                    "description": "Ping response.",
+                },
+                "RenamedPingRequest": {
+                    "properties": {
+                        "req_foo": {"type": "string", "title": "Req Foo", "description": "foo value of the request"},
+                        "req_bar": {"type": "string", "title": "Req Bar", "description": "bar value of the request"},
+                    },
+                    "type": "object",
+                    "required": ["req_foo", "req_bar"],
+                    "title": "RenamedPingRequest",
+                    "description": "Ping Request.",
+                },
+                "RenamedPingResponse": {
+                    "properties": {
+                        "resp_foo": {"type": "string", "title": "Resp Foo", "description": "foo value of the response"},
+                        "resp_bar": {"type": "string", "title": "Resp Bar", "description": "bar value of the response"},
+                    },
+                    "type": "object",
+                    "required": ["resp_foo", "resp_bar"],
+                    "title": "RenamedPingResponse",
+                    "description": "Ping response.",
+                },
+            }
+        },
+    }
 
 
-class PingResponse(BaseModel):
-    """Ping response."""
-
-    resp_foo: str = Field(description="foo value of the response")
-    resp_bar: str = Field(description="bar value of the response")
-
-
-class PongResponse(BaseModel):
-    """Pong response."""
-
-    resp_foo: str = Field(alias="pong_foo", description="foo value of the response")
-    resp_bar: str = Field(alias="pong_bar", description="bar value of the response")
+def test_should_not_modify_schema_when_no_pydantic_models_are_present() -> None:
+    open_api = OpenAPI(
+        info=Info(
+            title="My own API",
+            version="v0.0.1",
+        ),
+        paths={},
+    )
+    assert construct_open_api_with_schema_class(open_api) == open_api
